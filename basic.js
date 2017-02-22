18ce0d85760882977256dd769ab7c2f9027099dd
unknown <nandakishorenw@gmail.com>
2017-02-22 19:24:37 +0530

    test component to publish

---
+++ basic.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class basicComponent extends React.Component {
+  render() {
+    return (
+      <div> hey wtp! test component </div>
+    )
+  }
+}
+export default basicComponent;
